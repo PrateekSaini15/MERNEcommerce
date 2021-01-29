64815e801acc134511bf8f176dcc249aaf87136c
Prateek Saini <prateeksaini15@yahoo.com>
2021-01-29 07:27:42 +0530

    Create user schema

---
+++ Backend/models/user.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 30,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 30,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    hashPassword: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "admin",
+    },
+    contactNumber: {
+      type: String,
+    },
+    profilePicture: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
