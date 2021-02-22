d4441e55eb6fe4f78331689fe735b18876ef4b16
Prateek Saini <prateeksaini15@yahoo.com>
2021-02-22 19:13:59 +0530

    Add functionality to add new entry in product inventory in frontend

--- frontend/src/redux/actions/inventoryActions.js
+++ frontend/src/redux/actions/inventoryActions.js
@@ -1,4 +1,4 @@
-import { GET_PRODUCT_INVENTORY } from "./actionTypes";
+import { GET_PRODUCT_INVENTORY, ADD_ENTRY_TO_INVENTORY } from "./actionTypes";
 import axios from "../axios";
 
 axios.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
@@ -12,6 +12,15 @@ export const getProductInventory = (productId) => async (dispatch) => {
       type: GET_PRODUCT_INVENTORY,
       payload: res.data,
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addEntryToInventory = (entry) => async (dispatch) => {
+  try {
+    const res = await axios.post("/api/merchant/inventory/add", entry);
+    dispatch({ type: ADD_ENTRY_TO_INVENTORY, payload: res.data });
   } catch (error) {
     console.log(error);
   }
--- frontend/src/routes/Admin/ShowInventory/ShowInventory.js
+++ frontend/src/routes/Admin/ShowInventory/ShowInventory.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getProductInventory } from "../../../redux/actions/inventoryActions";
-
+import {
+  getProductInventory,
+  addEntryToInventory,
+} from "../../../redux/actions/inventoryActions";
+import NewInvetoryEntry from "../../../components/NewInventoryEntry/NewInventoryEntry";
 class ShowInventory extends React.Component {
   constructor(props) {
     super(props);
-    this.getMarkup = this.getMarkup.bind(this);
+    this.state = {
+      getNewEntry: false,
+    };
+    this.getBodyMarkup = this.getBodyMarkup.bind(this);
     this.createRow = this.createRow.bind(this);
     this.formatDate = this.formatDate.bind(this);
+    this.getFinalRowMarkup = this.getFinalRowMarkup.bind(this);
+    this.toggleGetNewEntry = this.toggleGetNewEntry.bind(this);
+    this.toggleGetNewEntry2 = this.toggleGetNewEntry2.bind(this);
   }
   componentDidMount() {
     this.props.getProductInventory(this.props.location.state._id);
+  }
+
+  toggleGetNewEntry2() {
+    console.log(this.state);
   }
 
   formatDate(date) {
@@ -28,15 +41,36 @@ class ShowInventory extends React.Component {
     );
   }
 
-  getMarkup() {
+  getBodyMarkup() {
     const entries = this.props.entries;
     const markup = entries.map(this.createRow);
     return markup;
   }
 
+  toggleGetNewEntry() {
+    this.setState((prevState) => ({
+      prevState,
+      getNewEntry: !prevState.getNewEntry,
+    }));
+  }
+
+  getFinalRowMarkup() {
+    return (
+      <tr>
+        <td></td>
+        <td>
+          <button className="btn" onClick={this.toggleGetNewEntry}>
+            <i className="bi bi-clipboard-plus" style={{ color: "green" }}></i>
+          </button>
+        </td>
+      </tr>
+    );
+  }
+
   render() {
     const name = this.props.location.state.name;
-    const markup = this.getMarkup();
+    const bodyMarkup = this.getBodyMarkup();
+    const finalRowMarkup = this.getFinalRowMarkup();
     return (
       <>
         <h4 className="display-4 text-center">Inventory</h4>
@@ -48,8 +82,18 @@ class ShowInventory extends React.Component {
               <th scope="col">Stock Quantity</th>
             </tr>
           </thead>
-          <tbody>{markup}</tbody>
+          <tbody>
+            {bodyMarkup}
+            {finalRowMarkup}
+          </tbody>
         </table>
+        {this.state.getNewEntry ? (
+          <NewInvetoryEntry
+            productId={this.props.location.state._id}
+            addEntryToInventory={this.props.addEntryToInventory}
+            toggle={this.toggleGetNewEntry}
+          />
+        ) : null}
       </>
     );
   }
@@ -61,6 +105,7 @@ function mapStateToProps(store) {
 
 const mapActionToProps = {
   getProductInventory,
+  addEntryToInventory,
 };
 
 export default connect(mapStateToProps, mapActionToProps)(ShowInventory);
