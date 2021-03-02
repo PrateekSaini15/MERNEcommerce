a8c8e597522653193e7f0a44f33697c32081f290
Prateek Saini <prateeksaini15@yahoo.com>
2021-03-02 17:07:34 +0530

    Add the proxy for development

--- frontend/src/redux/axios.js
+++ frontend/src/redux/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-});
+const axiosInstance = axios.create();
 
 export function getAxiosInstance(user = "") {
   if (user === "user") {
